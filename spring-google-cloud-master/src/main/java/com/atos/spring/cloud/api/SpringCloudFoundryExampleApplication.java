ff49a887a1893e9635647c3d271b04af57974f27
ramgandham15 <42109419+ramgandham15@users.noreply.github.com>
2020-05-06 12:10:45 +0530

    gcp app engine integeration
    gcp app engine integeration

---
+++ spring-google-cloud-master/src/main/java/com/atos/spring/cloud/api/SpringCloudFoundryExampleApplication.java
@@ -0,0 +1,19 @@
+package com.atos.spring.cloud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class SpringCloudFoundryExampleApplication extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFoundryExampleApplication.class, args);
+	}
+}
